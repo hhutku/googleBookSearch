5b8162e719c4a8406619fb972b5f375d3139c17e
hhutku <hhutku.wa@gmail.com>
2021-03-22 14:16:24 -0700

    scrips added

---
+++ scripts/db.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const GoogleBooks = require("../controllers/booksController");
+const db = require("../models");
+mongoose.connect(
+  process.env.MONGODB_URI ||
+  "mongodb://localhost/googlebooks"
+);
+
+
+
+const books = {
+  title: "title12",
+  authors: "hemigway12",
+  description: "descriptionsss12",
+  image: "image link12",
+  link: "linkkk12"
+
+}
+
+const saveBook = async ()=> {
+ const book=await db.GoogleBooks.create(books);
+ console.log(book) 
+ 
+
+}
+
+saveBook();
+
