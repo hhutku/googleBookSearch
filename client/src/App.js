986a5e888773fb07ae396f47ccd57c312b831234
hhutku <hhutku.wa@gmail.com>
2021-03-22 01:52:06 -0700

    Nav added

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,15 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Saved from "./pages/Saved";
 import NotFound from "./pages/NotFound";
-import GoogleState from "./context/google/GoogleState"
+import GoogleState from "./context/google/GoogleState";
+import Nav from "./components/Nav/Nav"
 
 
 function App() {
   return (
     <GoogleState>
     <Router>
+      <Nav/>
     <div>
       <Switch>
         <Route exact path="/" component={Home} />
