5bdc2dd916cfb9eccd43f017e63866f7e3d51439
hhutku <hhutku.wa@gmail.com>
2021-03-23 14:52:40 -0700

    deploy

---
+++ client/build/precache-manifest.8ffd28425da49b42d9974ab7e075c441.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65c7bf77e8dc274cbeedaf7da73cc691",
+    "url": "/index.html"
+  },
+  {
+    "revision": "416213650a7817f2207b",
+    "url": "/static/css/main.86e3245e.chunk.css"
+  },
+  {
+    "revision": "598364d97ca6d0c8c701",
+    "url": "/static/js/2.80921573.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.80921573.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "416213650a7817f2207b",
+    "url": "/static/js/main.9ee42ba4.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  },
+  {
+    "revision": "590ec728080b730988342051f68d03a7",
+    "url": "/static/media/bookshelf.590ec728.jpg"
+  }
+]);
