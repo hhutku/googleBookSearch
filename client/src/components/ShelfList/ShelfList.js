02bec439fdd48dd3390edf2c1894706cdb48f871
hhutku <hhutku.wa@gmail.com>
2021-03-23 09:05:10 -0700

    removeBook update

--- client/src/components/ShelfList/ShelfList.js
+++ client/src/components/ShelfList/ShelfList.js
@@ -6,6 +6,10 @@ const ShelfList= ()=>  {
     const shelfContext = useContext(ShelfContext);
     const booksInShelf= shelfContext.books
 
+    const removeBook=(id)=>{
+        console.log(id)
+        shelfContext.removeBookFromShelf(id)
+    }
         return (
             booksInShelf.map((book) => (
             <ShelfListItem 
@@ -16,6 +20,7 @@ const ShelfList= ()=>  {
             authors={book.authors && book.authors.length > 1 ? book.authors.join(",") : book.authors}
             image={book.image ? book.image : "https://previews.123rf.com/images/pavelstasevich/pavelstasevich1811/pavelstasevich181101065/112815953-no-image-available-icon-flat-vector.jpg"}
             description={book.description}
+            removeBook={removeBook}
            
             />
         ))
--- client/src/components/ShelfList/ShelfListItem.js
+++ client/src/components/ShelfList/ShelfListItem.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
 function ShelfListItem(props) {
-    const {title, authors, image, link, description} = props
+    const {title, authors, image, link, description,removeBook,id} = props
 
-const removeBook=()=>{
-    console.log("remove----")
-}
+
 
     return (
         <div>
@@ -17,7 +15,7 @@ const removeBook=()=>{
                     <p className="card-text" >{description}</p>
                     <p style={{fontStyle: "italic"}}>Author(s): {authors}</p>
                     <a href={link} target="_blank" rel="noopener noreferrer" className="btn" style={{marginRight: "6px", backgroundColor: "rgb(33, 150, 243)", color: "white"}}>View Book</a>
-                    <button onClick={removeBook} className="btn btn-primary">Delete</button>
+                    <button onClick={()=>removeBook(id)} className="btn btn-primary">Delete</button>
                 </div>
             </div>
         </div>
