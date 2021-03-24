c310c68cda426249263fe401e8308e5b2e64b1b7
hhutku <hhutku.wa@gmail.com>
2021-03-24 00:28:04 -0700

    bug fix

--- models/book.js
+++ models/book.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-    id: {type: String, require: true},
+    id: {type: String, unique: true, require: true},
     title: { type: String, require: true},
     authors: [{ type: String, require: true}],
     description: { type: String, require: true},
